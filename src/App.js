5e14ef1d3c658744fcfa4b564fb536a2bde0d7fc
Joel V Jose <joelvjoseadoor@gmail.com>
2024-11-15 01:58:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    advice: "",
+    isTyping: false,
+  };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+        this.startTypingEffect(advice);
+      })
+      .catch((error) => {
+        console.error(error);
+        this.setState({ advice: "Oops! Something went wrong.", isTyping: false });
+      });
+  };
+
+  startTypingEffect = (quote) => {
+    this.setState({ isTyping: true });
+    let i = 0;
+    let text = "";
+    const interval = setInterval(() => {
+      text += quote[i];
+      this.setState({ advice: text });
+      i++;
+      if (i === quote.length) clearInterval(interval);
+    }, 50);
+  };
+
+  render() {
+    const { advice, isTyping } = this.state;
+
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className={`heading ${isTyping ? "typing" : ""}`}>{advice}</h1>
+
+          {/* Button to fetch new advice */}
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>Give Me Advice</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
